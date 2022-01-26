d3c55fc204dbea59939de3f1f774570bfb80d470
kswen23 <89299245+kswen23@users.noreply.github.com>
2022-01-26 16:56:04 +0900

    Revert "mainCoordinator생성"
    This reverts commit 4c278af7533dd1fb88e774cb0bdd8cf8c1cb80e3.

---
+++ DangProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DangProject
+//
+//  Created by 김동우 on 2021/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //test
+    }
+//23123
+
+}
+
