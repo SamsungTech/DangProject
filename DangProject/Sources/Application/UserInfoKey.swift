9bb23e21e7ab956b09a8935c40e086ff08dde713
김성원 <kswen0203@gmail.com>
2022-06-13 16:58:19 +0900

    refactor: userInfoKey getUserDefaultUID 업데이트

--- DangProject/Sources/Application/UserInfoKey.swift
+++ DangProject/Sources/Application/UserInfoKey.swift
@@ -1,3 +1,8 @@
+import Foundation
+
 struct UserInfoKey {
     static let firebaseUID = "FirebaseUID"
+    static let getUserDefaultsUID: String = {
+        UserDefaults.standard.string(forKey: UserInfoKey.firebaseUID)
+    }() ?? ""
 }
