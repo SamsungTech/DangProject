0c0e36fd498cbaae561141e438e3684b3427df89
김성원 <kswen0203@gmail.com>
2022-07-26 16:41:22 +0900

    chore: 초기 데이터 AlarmDomainModel에 정리

--- DangProject/Sources/Domain Layer/Models/AlarmDomainModel.swift
+++ DangProject/Sources/Domain Layer/Models/AlarmDomainModel.swift
@@ -39,6 +39,48 @@ struct AlarmDomainModel {
         self.identifier = alarmTableViewCellViewModel.identifier
     }
     
+    init(alarmEntity: Alarm) {
+        self.isOn = alarmEntity.isOn
+        self.title = alarmEntity.title
+        self.message = alarmEntity.message
+        self.time = alarmEntity.time
+        self.selectedDaysOfTheWeek = alarmEntity.selectedDays
+        self.identifier = alarmEntity.identifier
+    }
+    
+    static let initialAlarmDomainModel: [Self] = [
+        AlarmDomainModel(isOn: false,
+                    title: "아침식사",
+                    message: "아침먹고 기록",
+                    time: .makeTime(hour: 8, minute: 0),
+                    selectedDaysOfTheWeek: [1,2,3,4,5,6,7]),
+        AlarmDomainModel(isOn: false,
+                    title: "아침식사",
+                    message: "",
+                    time: .makeTime(hour: 9, minute: 0),
+                    selectedDaysOfTheWeek: [6,7]),
+        AlarmDomainModel(isOn: false,
+                    title: "점심식사",
+                    message: "점심먹었다",
+                    time: .makeTime(hour: 11, minute: 0),
+                    selectedDaysOfTheWeek: [1,2,3,4,5]),
+        AlarmDomainModel(isOn: false,
+                    title: "점심식사",
+                    message: "",
+                    time: .makeTime(hour: 12, minute: 0),
+                    selectedDaysOfTheWeek: [2,3,4,5,6]),
+        AlarmDomainModel(isOn: false,
+                    title: "점심식사",
+                    message: "",
+                    time: .makeTime(hour: 13, minute: 0),
+                    selectedDaysOfTheWeek: [3,4,7]),
+        AlarmDomainModel(isOn: false,
+                    title: "점심식사",
+                    message: "",
+                    time: .makeTime(hour: 16, minute: 0),
+                    selectedDaysOfTheWeek: [1,2,3,7]),
+    ]
+    
     static func makeDefaultAlarmIdentifier(title: String, time: Date) -> String {
         let alarmInformation = "\(title) \(String.timeToStringWith24Hour(time))"
         let currentTimeString: String = .currentDateToString()
