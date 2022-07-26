0cd8e22113f03c7ff061c22370fec7398d87e5e1
김성원 <kswen0203@gmail.com>
2022-07-26 16:44:14 +0900

    chore: UserInfoKey 추가

--- DangProject/Sources/Application/UserInfoKey.swift
+++ DangProject/Sources/Application/UserInfoKey.swift
@@ -1,4 +1,5 @@
 struct UserInfoKey {
     static let firebaseUID = "FirebaseUID"
     static let userNotificationsPermission = "UserNotificationsPermission"
+    static let initialAlarmDataIsNeeded = "InitialAlarmDataIsNeeded"
 }
