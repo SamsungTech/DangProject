0c90a45f554174d68f7307923e87a580d6ab3c5e
김성원 <kswen0203@gmail.com>
2022-08-01 16:48:42 +0900

    refactor: temAlamData -> alarmData 외 코드 가독성 보완

--- DangProject/Sources/Presentation Layer/Setting/Alarm/ViewModel/AlarmViewModel.swift
+++ DangProject/Sources/Presentation Layer/Setting/Alarm/ViewModel/AlarmViewModel.swift
@@ -46,7 +46,7 @@ class AlarmViewModel: AlarmViewModelProtocol {
     private let disposeBag = DisposeBag()
     
     var alarmDataArrayRelay = BehaviorRelay<[AlarmTableViewCellViewModel]>(value: [])
-    lazy var tempAlarmData: [AlarmTableViewCellViewModel] = { alarmDataArrayRelay.value }()
+    lazy var alarmData: [AlarmTableViewCellViewModel] = { alarmDataArrayRelay.value }()
     lazy var cellScaleWillExpand: Bool = false
     lazy var addedCellIndex: Int = 0
     lazy var changedCellIndex: Int = 0
@@ -63,45 +63,44 @@ class AlarmViewModel: AlarmViewModelProtocol {
         alarmManagerUseCase.alarmDomainModelsRelay
             .map { $0.map { AlarmTableViewCellViewModel.init(alarmDomainModel: $0) } }
             .subscribe(onNext: { [weak self] data in
-                guard let self = self else { return }
-                self.alarmDataArrayRelay.accept(data)
+                self?.alarmDataArrayRelay.accept(data)
             })
             .disposed(by: disposeBag)
     }
 
     // MARK: - Input
     func checkUserNotificationsIsFirst() {
-        if !UserDefaults.standard.bool(forKey: UserInfoKey.userNotificationsPermission) {
+        if UserDefaults.standard.bool(forKey: UserInfoKey.userNotificationsPermission) == false {
             alarmManagerUseCase.getRequestAuthorization()
         }
     }
     
     func expandSelectedCell(index: Int) {
         resetTotalCellScaleNormal(index: index)
-        switch tempAlarmData[index].scale {
+        switch alarmData[index].scale {
         case .normal:
-            tempAlarmData[index].scale = .moreExpand
+            alarmData[index].scale = .moreExpand
         case .expand:
-            tempAlarmData[index].scale = .expand
+            alarmData[index].scale = .expand
         case .moreExpand:
-            tempAlarmData[index].scale = .moreExpand
+            alarmData[index].scale = .moreExpand
         }
-        alarmDataArrayRelay.accept(tempAlarmData)
+        alarmDataArrayRelay.accept(alarmData)
     }
     
     func changeCellActivated(index: Int) {
         resetTotalCellScaleNormal(index: index)
         
-        switch tempAlarmData[index].scale {
+        switch alarmData[index].scale {
         case .normal:
-            tempAlarmData[index].scale = .moreExpand
-            self.cellScaleWillExpand = true
+            alarmData[index].scale = .moreExpand
+            cellScaleWillExpand = true
         case .expand, .moreExpand:
-            tempAlarmData[index].scale = .normal
-            self.cellScaleWillExpand = false
+            alarmData[index].scale = .normal
+            cellScaleWillExpand = false
         }
         
-        alarmDataArrayRelay.accept(tempAlarmData)
+        alarmDataArrayRelay.accept(alarmData)
     }
     
     func addAlarmDomainModel(_ alarmDomainModel: AlarmDomainModel) {
@@ -109,63 +108,59 @@ class AlarmViewModel: AlarmViewModelProtocol {
         alarmViewModel.scale = .moreExpand
         alarmManagerUseCase.changeAlarmNotificationRequest(data: alarmViewModel, changedOption: .add)
         
-        tempAlarmData.append(alarmViewModel)
-        tempAlarmData = tempAlarmData.sorted { $0.time < $1.time }
-        guard let index = self.tempAlarmData.firstIndex(of: alarmViewModel) else { return }
+        alarmData.append(alarmViewModel)
+        alarmData = alarmData.sorted { $0.time < $1.time }
+        guard let index = self.alarmData.firstIndex(of: alarmViewModel) else { return }
         addedCellIndex = index
         resetTotalCellScaleNormal(index: addedCellIndex)
-        alarmDataArrayRelay.accept(tempAlarmData)
+        alarmDataArrayRelay.accept(alarmData)
     }
     
     func changeIsOnValue(index: Int) {
-        tempAlarmData[index].isOn.toggle()
-        alarmManagerUseCase.changeAlarmNotificationRequest(data: tempAlarmData[index], changedOption: .isOn)
-        alarmDataArrayRelay.accept(tempAlarmData)
+        alarmData[index].isOn.toggle()
+        alarmManagerUseCase.changeAlarmNotificationRequest(data: alarmData[index], changedOption: .isOn)
+        alarmDataArrayRelay.accept(alarmData)
     }
     
     func changeUserMessage(index: Int, text: String) {
-        tempAlarmData[index].message = text
-        alarmManagerUseCase.changeAlarmNotificationRequest(data: tempAlarmData[index], changedOption: .message)
-        alarmDataArrayRelay.accept(tempAlarmData)
+        alarmData[index].message = text
+        alarmManagerUseCase.changeAlarmNotificationRequest(data: alarmData[index], changedOption: .message)
+        alarmDataArrayRelay.accept(alarmData)
     }
     
     func changeTime(index: Int, time: Date) {
-        tempAlarmData[index].time = time
-        tempAlarmData[index].timeText = .timeToString(time)
-        tempAlarmData[index].amPm = .timeToAmPm(time)
-        alarmManagerUseCase.changeAlarmNotificationRequest(data: tempAlarmData[index], changedOption: .time)
+        alarmData[index].time = time
+        alarmData[index].timeText = .timeToString(time)
+        alarmData[index].amPm = .timeToAmPm(time)
+        alarmManagerUseCase.changeAlarmNotificationRequest(data: alarmData[index], changedOption: .time)
         
-        let willChangeAlarmData = tempAlarmData[index]
-        tempAlarmData = tempAlarmData.sorted { $0.time < $1.time }
-        guard let dataIndex = self.tempAlarmData.firstIndex(of: willChangeAlarmData) else { return }
+        let willChangeAlarmData = alarmData[index]
+        alarmData = alarmData.sorted { $0.time < $1.time }
+        guard let dataIndex = self.alarmData.firstIndex(of: willChangeAlarmData) else { return }
         changedCellIndex = dataIndex
-        alarmDataArrayRelay.accept(tempAlarmData)
+        alarmDataArrayRelay.accept(alarmData)
     }
     
     func changeDayOfTheWeek(index: Int, tag: Int) {
         guard let selectedTag = DayOfWeek(rawValue: tag) else { return }
-        if tempAlarmData[index].selectedDaysOfWeek.contains(selectedTag) {
-            guard let arrayIndex = tempAlarmData[index].selectedDaysOfWeek.firstIndex(of: selectedTag) else { return }
-            tempAlarmData[index].selectedDaysOfWeek.remove(at: arrayIndex)
+        if alarmData[index].selectedDaysOfWeek.contains(selectedTag) {
+            guard let arrayIndex = alarmData[index].selectedDaysOfWeek.firstIndex(of: selectedTag) else { return }
+            alarmData[index].selectedDaysOfWeek.remove(at: arrayIndex)
         } else {
-            tempAlarmData[index].selectedDaysOfWeek.append(selectedTag)
-            tempAlarmData[index].selectedDaysOfWeek = tempAlarmData[index].selectedDaysOfWeek.sorted{ $0.rawValue < $1.rawValue }
+            alarmData[index].selectedDaysOfWeek.append(selectedTag)
+            alarmData[index].selectedDaysOfWeek = alarmData[index].selectedDaysOfWeek.sorted{ $0.rawValue < $1.rawValue }
         }
         calculateEveryDayAndSelectedDays(index: index)
-        alarmManagerUseCase.changeAlarmNotificationRequest(data: tempAlarmData[index], changedOption: .dayOfWeek)
-        alarmDataArrayRelay.accept(tempAlarmData)
+        alarmManagerUseCase.changeAlarmNotificationRequest(data: alarmData[index], changedOption: .dayOfWeek)
+        alarmDataArrayRelay.accept(alarmData)
     }
     
     func changeEveryday(index: Int) {
-        if tempAlarmData[index].isEveryDay {
-            tempAlarmData[index].scale = .moreExpand
-        } else {
-            tempAlarmData[index].scale = .expand
-        }
-        tempAlarmData[index].isEveryDay.toggle()
+        alarmData[index].scale = alarmData[index].isEveryDay ? .moreExpand : .expand
+        alarmData[index].isEveryDay.toggle()
         calculateDaysOfWeekAndSelectedDays(index: index)
-        alarmManagerUseCase.changeAlarmNotificationRequest(data: tempAlarmData[index], changedOption: .isEveryDay)
-        alarmDataArrayRelay.accept(tempAlarmData)
+        alarmManagerUseCase.changeAlarmNotificationRequest(data: alarmData[index], changedOption: .isEveryDay)
+        alarmDataArrayRelay.accept(alarmData)
     }
     
     func willDeleteAlarmData(_ indexPath: Int) {
@@ -173,9 +168,9 @@ class AlarmViewModel: AlarmViewModelProtocol {
     }
     
     func deleteAlarmData() {
-        alarmManagerUseCase.changeAlarmNotificationRequest(data: tempAlarmData[willDeleteCellIndex], changedOption: .delete)
-        tempAlarmData.remove(at: willDeleteCellIndex)
-        alarmDataArrayRelay.accept(tempAlarmData)
+        alarmManagerUseCase.changeAlarmNotificationRequest(data: alarmData[willDeleteCellIndex], changedOption: .delete)
+        alarmData.remove(at: willDeleteCellIndex)
+        alarmDataArrayRelay.accept(alarmData)
     }
 
     // MARK: - Output
@@ -193,21 +188,21 @@ class AlarmViewModel: AlarmViewModelProtocol {
         
     // MARK: - Private
     private func calculateDaysOfWeekAndSelectedDays(index: Int) {
-        tempAlarmData[index].selectedDaysOfWeek = AlarmTableViewCellViewModel.calculateDaysOfWeek(tempAlarmData[index].isEveryDay)
-        tempAlarmData[index].selectedDays = AlarmTableViewCellViewModel.calculateSelectedDays(tempAlarmData[index].selectedDaysOfWeek)
-        tempAlarmData[index].isOn = AlarmTableViewCellViewModel.calculateIsOn(days: tempAlarmData[index].selectedDaysOfWeek, origin: tempAlarmData[index].isOn)
+        alarmData[index].selectedDaysOfWeek = AlarmTableViewCellViewModel.calculateDaysOfWeek(alarmData[index].isEveryDay)
+        alarmData[index].selectedDays = AlarmTableViewCellViewModel.calculateSelectedDays(alarmData[index].selectedDaysOfWeek)
+        alarmData[index].isOn = AlarmTableViewCellViewModel.calculateIsOn(days: alarmData[index].selectedDaysOfWeek, origin: alarmData[index].isOn)
     }
     
     private func calculateEveryDayAndSelectedDays(index: Int) {
-        tempAlarmData[index].isEveryDay = AlarmTableViewCellViewModel.calculateEveryDay(tempAlarmData[index].selectedDaysOfWeek)
-        tempAlarmData[index].selectedDays = AlarmTableViewCellViewModel.calculateSelectedDays(tempAlarmData[index].selectedDaysOfWeek)
-        tempAlarmData[index].isOn = AlarmTableViewCellViewModel.calculateIsOn(days: tempAlarmData[index].selectedDaysOfWeek, origin: tempAlarmData[index].isOn)
+        alarmData[index].isEveryDay = AlarmTableViewCellViewModel.calculateEveryDay(alarmData[index].selectedDaysOfWeek)
+        alarmData[index].selectedDays = AlarmTableViewCellViewModel.calculateSelectedDays(alarmData[index].selectedDaysOfWeek)
+        alarmData[index].isOn = AlarmTableViewCellViewModel.calculateIsOn(days: alarmData[index].selectedDaysOfWeek, origin: alarmData[index].isOn)
     }
     
     private func resetTotalCellScaleNormal(index: Int) {
-        for i in 0 ..< tempAlarmData.count {
+        for i in 0 ..< alarmData.count {
             if i != index {
-                tempAlarmData[i].scale = .normal
+                alarmData[i].scale = .normal
             }
         }
     }
