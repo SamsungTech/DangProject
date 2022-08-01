853cf4869c1baa99b75190fe7d68a9203efac7ab
김성원 <kswen0203@gmail.com>
2022-08-02 00:26:26 +0900

    chore: UserInfoKey.isFirstTime -> UseInfoKey.tutorialFinished 네이밍 변경

--- DangProject/Sources/Application/UserInfoKey.swift
+++ DangProject/Sources/Application/UserInfoKey.swift
@@ -2,7 +2,7 @@ import Foundation
 
 struct UserInfoKey {
     static let firebaseUID = "FirebaseUID"
-    static let isFirstTime = "Onboarding"
+    static let tutorialFinished = "TutorialFinished"
     static let getUserDefaultsUID: String = {
         UserDefaults.standard.string(forKey: UserInfoKey.firebaseUID)
     }() ?? ""
