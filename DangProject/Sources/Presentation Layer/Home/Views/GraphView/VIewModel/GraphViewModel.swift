6f83881d9847003484df385100ef707db843488e
김성원 <kswen0203@gmail.com>
2022-08-23 00:47:44 +0900

    feat: 6개월간 평균 섭취 당 계산하는 메서드 추가 - calculateMonthlySugarAverage()

--- DangProject/Sources/Presentation Layer/Home/Views/GraphView/VIewModel/GraphViewModel.swift
+++ DangProject/Sources/Presentation Layer/Home/Views/GraphView/VIewModel/GraphViewModel.swift
@@ -15,7 +15,7 @@ protocol GraphViewModelInputProtocol {
 }
 
 protocol GraphViewModelOutputProtocol {
-    
+
 }
 
 protocol GraphViewModelProtocol: GraphViewModelInputProtocol, GraphViewModelOutputProtocol { }
@@ -23,10 +23,47 @@ protocol GraphViewModelProtocol: GraphViewModelInputProtocol, GraphViewModelOutp
 class GraphViewModel: GraphViewModelProtocol {
     
     private let disposeBag = DisposeBag()
+    
     private let fetchEatenFoodsUseCase: FetchEatenFoodsUseCase
     
     init(fetchEatenFoodsUseCase: FetchEatenFoodsUseCase) {
         self.fetchEatenFoodsUseCase = fetchEatenFoodsUseCase
+        bindMonthlyTotalSugarObservable()
+    }
+    
+    private func bindMonthlyTotalSugarObservable() {
+        fetchEatenFoodsUseCase.sixMonthsTotalSugarObservable
+            .subscribe(onNext: { [weak self] monthlyTotalSugar in
+                let monthlyAverage = self?.calculateMonthlySugarAverage(monthlyTotalSugar)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func calculateMonthlySugarAverage(_ monthlyTotalSugar: [TotalSugarPerMonthDomainModel]) -> [Double] {
+        var currentDateComponents: DateComponents = .currentYearMonth()
+        currentDateComponents.day = 1
+        var monthlySugarAverage = [Double]()
+        for i in 0 ..< monthlyTotalSugar.count {
+            var totalSugar: Double = 0
+            var dayCount: Int = 0
+            
+            if monthlyTotalSugar[i].month == currentDateComponents {
+                dayCount = DateComponents.currentDateComponents().day!
+                for day in 0 ..< dayCount {
+                    totalSugar = totalSugar + monthlyTotalSugar[i].totalSugarPerMonth[day].totalSugar
+                }
+            } else {
+                dayCount = monthlyTotalSugar[i].totalSugarPerMonth.count
+                monthlyTotalSugar[i].totalSugarPerMonth.forEach {
+                    totalSugar = totalSugar + $0.totalSugar
+                }
+            }
+            
+            let sugarAverage = totalSugar / Double(dayCount)
+            monthlySugarAverage.append(sugarAverage.roundDecimal(to: 1))
+        }
+        
+        return monthlySugarAverage
     }
     
 }
